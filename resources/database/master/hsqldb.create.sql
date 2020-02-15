611756977f8e83ec2b2d045fc9d6be94c513865f
eluinstra <e.luinstra@gmail.com>
2020-02-15 17:10:32 +0100

    added timestamp to fs_file fixed number type

--- resources/database/master/hsqldb.create.sql
+++ resources/database/master/hsqldb.create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE fs_client
 (
-	id								NUMBER(10)			NOT NULL PRIMARY KEY,
+	id								INTEGER					NOT NULL PRIMARY KEY,
 	name							VARCHAR(256)		NOT NULL UNIQUE,
 	certificate				BLOB						NOT NULL
 );
@@ -10,8 +10,9 @@ CREATE TABLE fs_file
 	virtual_path			VARCHAR(256)		NOT NULL PRIMARY KEY,
 	real_path					VARCHAR(256)		NOT NULL,
 	content_type			VARCHAR(256)		NOT NULL,
+	timestamp					TIMESTAMP				DEFAULT NOW NOT NULL,
 	start_date				TIMESTAMP				NOT NULL,
 	end_date					TIMESTAMP				NOT NULL,
-	client_id					NUMBER(10)			NOT NULL,
-	FOREIGN KEY (client_id) REFERENCES client(id)
+	client_id					INTEGER					NOT NULL,
+	FOREIGN KEY (client_id) REFERENCES fs_client(id)
 );
