cd219aa5b47c4f9196dd5c008b22cffe3ebd2449
eluinstra <e.luinstra@gmail.com>
2020-07-04 20:25:41 +0200

    added database scripts

---
+++ src/main/resources/org/bitbucket/eluinstra/fs/db/migration/mssql/V1.0.0__Initial.sql
@@ -0,0 +1,37 @@
+--
+-- Copyright 2020 E.Luinstra
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--   http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+CREATE TABLE client
+(
+	id								INT							IDENTITY(1,1)	PRIMARY KEY,
+	name							VARCHAR(256)		NOT NULL UNIQUE,
+	certificate				IMAGE						NOT NULL
+);
+
+CREATE TABLE file
+(
+	virtual_path			VARCHAR(256)		NOT NULL PRIMARY KEY,
+	real_path					VARCHAR(256)		NOT NULL,
+	filename					VARCHAR(256)		NOT NULL,
+	content_type			VARCHAR(256)		NOT NULL,
+	md5_checksum			VARCHAR(32)			NOT NULL,
+	sha256_checksum		VARCHAR(64)			NOT NULL,
+	timestamp					DATETIME				NOT NULL DEFAULT GETDATE(),
+	start_date				DATETIME				NULL,
+	end_date					DATETIME				NULL,
+	client_id					INT							NOT NULL,
+	FOREIGN KEY (client_id) REFERENCES client(id)
+);
---
+++ src/main/resources/org/bitbucket/eluinstra/fs/db/migration/mysql/V1.0.0__Initial.sql
@@ -0,0 +1,37 @@
+--
+-- Copyright 2020 E.Luinstra
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--   http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+CREATE TABLE client
+(
+	id								INTEGER					AUTO_INCREMENT PRIMARY KEY,
+	name							VARCHAR(256)		NOT NULL UNIQUE,
+	certificate				BLOB						NOT NULL
+);
+
+CREATE TABLE file
+(
+	virtual_path			VARCHAR(256)		NOT NULL PRIMARY KEY,
+	real_path					VARCHAR(256)		NOT NULL,
+	filename					VARCHAR(256)		NOT NULL,
+	content_type			VARCHAR(256)		NOT NULL,
+	md5_checksum			VARCHAR(32)			NOT NULL,
+	sha256_checksum		VARCHAR(64)			NOT NULL,
+	timestamp					TIMESTAMP				NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	start_date				TIMESTAMP				NULL,
+	end_date					TIMESTAMP				NULL,
+	client_id					INTEGER					NOT NULL,
+	FOREIGN KEY (client_id) REFERENCES client(id)
+);
